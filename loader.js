191981a21b78a80d6da3592169e55be2b7338cef
Zodac <devblueb@gmail.com>
2025-11-18 09:46:23 -0500

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,1 @@
-head.appendChild(createElement('script')).src='https://helloedlekidrfjiredkfjikes'
+head.appendChild(createElement('script')).src='https://github.com/ZodaciOS/Browser-Auto-Clicker/blob/main/main.js'
